65d537f14c259a5fd89582b9aa08fe97218f0e55
anasthezia <anasthezia161@gmail.com>
2019-04-14 12:14:23 +0300

    WIP guests list 2

--- src/components/GuestsList/index.js
+++ src/components/GuestsList/index.js
@@ -1,35 +1,29 @@
 import React from 'react';
-import { Table, Divider, Avatar } from 'antd';
+import {List, Avatar } from 'antd';
 import Guests from '../../data/guests';
-
-const columns = [{
-  title: 'image',
-  dataIndex: 'userpic_URL',
-  key: 'userpic_URL',
-  render: userpic_URL => <Avatar shape="circle" size={64} src={`/assets/images/fakedata/${userpic_URL}`}  />,
-
-}, 
-{
-  title: 'Name',
-  dataIndex: 'name',
-  // key: 'name',
-  render: name => <a href="javascript:;">{name}</a>,
-  rowSpan: 3,
-},
- {
-  title: 'Телефон',
-  dataIndex: 'phone',
-  // key: 'phone',
-}
-];
-
 
 class GuestsList extends React.Component {
   render() {
     return (
       <div>
     <h2>Список гостей</h2>
-    <Table columns={columns} dataSource={Guests} />
+    <List
+    itemLayout="horizontal"
+    dataSource={Guests}
+    renderItem={item => (
+      <List.Item>
+        <List.Item.Meta
+          avatar={ <Avatar shape="circle" size={64} src={`/assets/images/fakedata/${item.userpic_URL}`}  />}
+          title={item.name}
+          description={item.post}
+        />
+        <div>
+          {item.phone}
+          {item.username}
+        </div>
+      </List.Item>
+    )}
+  />,
       </div> 
     ) 
   }
