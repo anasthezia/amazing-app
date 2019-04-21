84d5cffe25dffb4f40f1f0e75e458fb484a4ea7c
anasthezia <anasthezia161@gmail.com>
2019-04-21 14:27:34 +0300

    TodoList - added  list view

--- src/components/Todo/index.js
+++ src/components/Todo/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 // import moment from 'moment';
-import { Calendar} from 'antd';
+import { Calendar, Table, Divider, Form, DatePicker, TimePicker, Button, Input} from 'antd';
 import TodoList from '../../data/todo-list';
 
 function getListData(value) {
@@ -40,14 +40,79 @@ function monthCellRender(value) {
 }
 
 
+const columns = [{
+  title: 'Дата',
+  dataIndex: 'date',
+  key: 'date',
+},  {
+  title: 'Content',
+  dataIndex: 'content',
+  key: 'content',
+}, 
+{
+  title: 'Action',
+  key: 'action',
+  render: (text, record) => (
+    <span>
+      <a href="javascript:;">Invite {record.name}</a>
+      <Divider type="vertical" />
+      <a href="javascript:;">Delete</a>
+    </span>
+  ),
+}];
+
+
+// const 
 
 
 class Todo extends React.Component {
+  handleSubmit = (e, values) => {
+    e.preventDefault();
+    console.log('Received values of form: ', values);
+  }
+
   render() {
-    return   <Calendar 
+
+    return (
+
+    <div> 
+            <Form  layout='inline'
+            onSubmit={this.handleSubmit}
+            >
+            <Form.Item
+        >
+           <DatePicker  format="DD-MM-YYYY" />
+           </Form.Item>
+          <Form.Item>
+             <TimePicker />
+          </Form.Item>
+           <Form.Item
+        >
+          <Input  placeholder="Что нужно сделать" />
+        </Form.Item>
+        <Form.Item>
+          <Button
+            type="primary"
+            htmlType="submit"
+          >
+            Добавить
+          </Button>
+        </Form.Item>
+</Form>
+      {/* <div className="selectView">
+      
+      </div>  */}
+      <Table 
+      columns={columns} 
+      dataSource={TodoList} 
+      rowClassName={(record, index) => ((record.done === true) && 'todo-row--done')}      />
+
+       <Calendar 
     dateCellRender={dateCellRender} 
     monthCellRender={monthCellRender} 
-    />
+    /> 
+      </div>
+    )  
   }
 }
 export default Todo
--- src/data/todo-list.js
+++ src/data/todo-list.js
@@ -2,62 +2,74 @@ const TodoList = [
     {
       id: 1,
       date:'May 1st 19',
-      content: 'Заказать баннер'
+      content: 'Заказать баннер',
+      done: false,
     },
     {
       id: 2,
       date:'Jun 17th 19',
-      content: 'Вырезать наклейки'
+      content: 'Вырезать наклейки',
+      done: true,
     },
     {
       id: 3,
       date:'Jun 3rd 19',
-      content: 'Подобрать плейлист'
+      content: 'Подобрать плейлист',
+      done: false,
     },
     {
       id: 4,
       date:'Jun 17th 19',
-      content: 'согласовать с ведущим конкурсы'
+      content: 'согласовать с ведущим конкурсы',
+      done: true,
     },
     {
       id: 5,
       date:'Jun 4th 19',
-      content: 'Подписать договор с автобусной компанией'
+      content: 'Подписать договор с автобусной компанией',
+      done: false,
     },
     {
       id: 6,
       date:'May 12th 19',
-      content: 'Написать пост для иснаграмм'
+      content: 'Написать пост для иснаграмм',
+      done: false,
     },
     {
       id: 7,
       date:'Jun 5th 19',
-      content: 'Украсить беседку'
+      content: 'Украсить беседку',
+      done: true,
     },
     {
       id: 8,
       date:'May 9th 19',
-      content: 'Встреча с шашлычником для соглавования меню'
+      content: 'Встреча с шашлычником для соглавования меню',
+      done: false,
     },
       {
       id: 9,
       date:'May 2nd 19',
-      content: 'Съездить на оптовую базу за продуктами'
+      content: 'Съездить на оптовую базу за продуктами',
+      done: true,
     },
     {
       id: 10,
       date:'May 2nd 19',
-      content: 'купить скатерть'
+      content: 'купить скатерть',
+      done: true,
     },
       {
       id: 11,
       date:'May 3rd 19',
-      content: 'Съездить в спортмастер за спортреквизитом'
+      content: 'Съездить в спортмастер за спортреквизитом',
+      done: false,
     },
       {
       id: 12,
       date:'May 5th 19',
-      content: 'Монтаж звука и света'
+      content: 'Монтаж звука и света',
+      done: true,
     },
   ]
 
