21031dc55bd63a181ecb0fed4f89c11fa6b5ef7d
anasthezia <anasthezia161@gmail.com>
2019-05-08 22:18:28 +0300

    WIP remake side menu - awesome icons

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-import { Route, Link, Switch} from 'react-router-dom'
-import { Layout, Menu, Icon} from 'antd';
+import { Route, Link, Switch } from 'react-router-dom'
+import { Layout, Menu, Icon } from 'antd';
 import Drawer from '@material-ui/core/Drawer';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
@@ -13,6 +13,10 @@ import ListItem from '@material-ui/core/ListItem';
 import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemText from '@material-ui/core/ListItemText';
 import Divider from '@material-ui/core/Divider';
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faStroopwafel } from '@fortawesome/free-solid-svg-icons'
+
 
 import "antd/dist/antd.css";
 import './App.scss';
@@ -22,9 +26,11 @@ import Todo from './components/Todo';
 import GuestsList from './components/GuestsList';
 import Contacts from './components/Contacts';
 import Schedule from './components/Schedule';
+import Item from 'antd/lib/list/Item';
 
+library.add(faStroopwafel)
 // const { Sider } = Layout;
-const {Sider, Content} = Layout;
+const { Sider, Content } = Layout;
 const drawerWidth = 240;
 
 
@@ -32,20 +38,48 @@ const Main = () => (
   <main>
     <Switch>
       {/* <Route path="/" component={Home} /> */}
-        <Route path="/budget" component={Budget} />
-        <Route path="/todo" component={Todo} />
-        <Route path="/guests" component={GuestsList} />
-        <Route path="/contacts" component={Contacts} />
-        <Route path="/schedule" component={Schedule} />
+      <Route path="/budget" component={Budget} />
+      <Route path="/todo" component={Todo} />
+      <Route path="/guests" component={GuestsList} />
+      <Route path="/contacts" component={Contacts} />
+      <Route path="/schedule" component={Schedule} />
 
     </Switch>
   </main>
 )
 
+
+const MenuItems = [
+  {
+    title: 'План',
+    link: '/schedule',
+    icon: <FontAwesomeIcon icon="stroopwafel" />,
+  },
+  {
+    title: 'Список дел',
+    link: '/todo',
+    icon: <Icon type="bars" />,
+  },
+  {
+    title: 'Гости',
+    link: '/guests',
+    icon: <Icon type="team" />,
+  },
+  {
+    title: 'Бюджет',
+    link: '/budget',
+    icon: <Icon type="wallet" />,
+  },
+  {
+    title: 'Контакты',
+    link: '/contacts',
+    icon: <Icon type="phone" />,
+  },
+]
 class App extends Component {
 
-  state = { 
-    visible: false,    
+  state = {
+    visible: false,
     collapsed: false,
     open: false,
   };
@@ -74,108 +108,50 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-      <Layout>
-        <header className="App-header"/>
-        <Drawer
-          variant="permanent"
-          // className={classNames(classes.drawer, {
-          //  'drawerOpen': this.state.open,
-          //   'drawerClose': !this.state.open,
-          // })}
-          // classes={{
-          //   paper: classNames({
-          //     'drawerOpen': this.state.open,
-          //    'drawerClose': !this.state.open,
-          //   }),
-          // }}
-          open={this.state.open}
-        >
-          <div 
-          // className={classes.toolbar}
-          >
-            <IconButton onClick={this.handleDrawerClose}>
-            <ChevronLeftIcon />
-              {/* {theme.direction === 'rtl' ? <ChevronRightIcon /> : <ChevronLeftIcon />} */}
-            </IconButton>
-          </div>
-          <Divider />
-          <List>
-          <ListItem button>
-                <ListItemIcon><Icon type="schedule" /></ListItemIcon>
-                <ListItemText><Link to="/schedule">
-              <span>Расписание</span>
-              </Link></ListItemText>
-              </ListItem>
-
-
-            {/* {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
-              <ListItem button key={text}>
-                <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
-                <ListItemText primary={text} />
-              </ListItem>
-            ))} */}
-          </List>
-          {/* <Divider /> */}
-          {/* <List>
-            {['All mail', 'Trash', 'Spam'].map((text, index) => (
-              <ListItem button key={text}>
-                <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
-                <ListItemText primary={text} />
-              </ListItem>
-            ))}
-          </List> */}
-        </Drawer>
-
-
-        {/* <Sider
-          trigger={null}
-          collapsible
-          collapsed={this.state.collapsed}
-        >
-            <Icon
-              className="trigger"
-              type={this.state.collapsed ? 'left' : 'right'}
-              onClick={this.toggle}
-            />
-          <Menu theme="dark" mode="inline" defaultSelectedKeys={['1']}>
-            <Menu.Item key="1">
-            <Link to="/schedule">
-              <Icon type="schedule" />
-              <span>Расписание</span>
-              </Link>
-            </Menu.Item>
-            <Menu.Item key="2">
-            <Link to="/todo">
-              <Icon type="bars" />
-              <span>Спитсок дел</span>            
-            </Link>
-            </Menu.Item>
-            <Menu.Item key="3">
-            <Link to="guests">
-              <Icon type="team" />
-              <span>Гости</span>
-              </Link>
-            </Menu.Item>
-            <Menu.Item key="4">
-            <Link to="/budget">
-              <Icon type="wallet" />
-              <span>Бюджет</span>
-            </Link>
-            </Menu.Item>
-            <Menu.Item key="5">
-            <Link to="/contacts">
-              <Icon type="phone" />
-              <span>Контакты</span>
-              </Link>
-            </Menu.Item>
-          </Menu>
-        </Sider> */}
         <Layout>
-        <Content>
-          <Main/>
-        </Content>
-      </Layout>
-     
+          <header className="App-header" />
+          <Drawer
+            variant="permanent"
+            // className={classNames(classes.drawer, {
+            //  'drawerOpen': this.state.open,
+            //   'drawerClose': !this.state.open,
+            // })}
+            // classes={{
+            //   paper: classNames({
+            //     'drawerOpen': this.state.open,
+            //    'drawerClose': !this.state.open,
+            //   }),
+            // }}
+            open={this.state.open}
+          >
+            <div
+            // className={classes.toolbar}
+            >
+              <IconButton onClick={this.handleDrawerClose}>
+                <ChevronLeftIcon />
+                {/* {theme.direction === 'rtl' ? <ChevronRightIcon /> : <ChevronLeftIcon />} */}
+              </IconButton>
+            </div>
+            <Divider />
+            <List>
+              {
+                MenuItems.map((item, index) => (
+                  <ListItem button key={index}>
+                    <ListItemIcon>{item.icon}</ListItemIcon>
+                    <ListItemText><Link to={item.link}>
+                      <span>{item.title}</span>
+                    </Link></ListItemText>
+                  </ListItem>
+                ))
+              }
+            </List>
+          </Drawer>
+          <Layout>
+            <Content>
+              <Main />
+            </Content>
+          </Layout>
+
         </Layout>
       </div>
     );
