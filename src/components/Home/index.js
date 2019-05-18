f0af6c961d75708ed18dc60ccfa58cc8eb0a0613
anasthezia <anasthezia161@gmail.com>
2019-05-18 23:13:29 +0300

    homepaga animation

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import Grid from '@material-ui/core/Grid';
+import { Controller, Scene } from 'react-scrollmagic';
+import { Tween, Timeline } from 'react-gsap';
 
 import EventData from '../../data/event-data';
 import './style.scss';
@@ -10,70 +12,103 @@ var sectionStyle = {
 };
 
 
-class Home extends React.Component {
-    render() {
-        return (
-            <div className="root">
-                <section className="home" style={sectionStyle} >
 
-                    <Grid container>
-                        <Grid item xs={12}>
+const Home = () => (
+    <div className="root">
+        <section className="home" style={sectionStyle} >
+            <Grid container>
 
-                            <div className="home__screen" >
-                                <div className="home__wrapper-1" >
-                                    <h1 className="home__title" >{EventData.title}</h1>
-                                    <p className="home__description" >{EventData.describtion}</p>
-                                </div>
-                            </div>
-                        </Grid>
-                        <Grid item xs={12} sm={6}>
-                            <div className="event-data">
-                                <div className="event-data__row">
-                                    <div className="event-data__row-title">Дата: </div>
-                                    <div className="event-data__row-content">{EventData.date} </div>
-                                </div>
-                                <div className="event-data__row">
-                                    <div className="event-data__row-title">Время: </div>
-                                    <div className="event-data__row-content">{EventData.time} </div>
-                                </div>
-                            </div>
-                            <div className="event-data">
-                                <div className="event-data__row">
-                                    <div className="event-data__row-title">Место:</div>
-                                    <div className="event-data__row-content">{EventData.place} </div>
-                                </div>
-                            </div>
-                            <div className="event-data">
-                                <div className="event-data__row">
-                                    <div className="event-data__row-title">Кол-во гостей:</div>
-                                    <div className="event-data__row-content">{EventData.total_guests_count} </div>
-                                </div>
-                                <div className="event-data__row">
-                                    <div className="event-data__row-title">Планируемый бюджет:</div>
-                                    <div className="event-data__row-content">{EventData.budget} </div>
-                                </div>
-                            </div>
-                        </Grid>
-                        <Grid item xs={12} sm={6}>
-                            <div className="event-data">
-                                <h3 className="event-data__title">Ответственные</h3>
-                                {EventData.responsibles.map((item, index) =>
-                                    <div className="event-data__row">
-                                        <div className="event-data__row-title">
-                                            {item.title}
-                                        </div>
-                                        <div classNme="event-data__row-content">
-                                            {item.content}
-                                        </div>
-                                    </div>
-                                )}
-                            </div>
-                        </Grid>
-                    </Grid>
-                </section>
-            </div>
-        )
+                <Grid item xs={12}>
+                    <Controller>
+                        <Scene
+                            triggerHook="onLeave"
+                            duration={1000}
+                            pin
+                        >
+                            {(progress) => (
+                                <div className="sticky">
+                                    <Timeline totalProgress={progress} paused>
+                                        <Timeline
+                                            target={
 
-    }
-}
+                                                <div className="home__wrapper-1" >
+                                                    <h1 className="home__title" >{EventData.title}</h1>
+                                                    <p className="home__description" >{EventData.describtion}</p>
+                                                </div>
+
+                                            }
+                                        >
+                                            <Tween
+                                                from={{ opacity: 0}}
+                                                to={{ opacity: 1 }}
+                                            />
+                                            <Tween
+                                                to={{ x: '110%' }}
+                                            />
+                                        </Timeline>
+                                    </Timeline>
+                                </div>
+                            )}
+                        </Scene>
+                    </Controller>
+                </Grid>
+            </Grid>
+            <Grid container>
+                <Grid item xs={12} sm={6}>
+                    <div className="event-data">
+                        <div className="event-data__row">
+                            <div className="event-data__row-title">Дата: </div>
+                            <div className="event-data__row-content">{EventData.date} </div>
+                        </div>
+                        <div className="event-data__row">
+                            <div className="event-data__row-title">Время: </div>
+                            <div className="event-data__row-content">{EventData.time} </div>
+                        </div>
+                    </div>
+                    <div className="event-data">
+                        <div className="event-data__row">
+                            <div className="event-data__row-title">Дата: </div>
+                            <div className="event-data__row-content">{EventData.date} </div>
+                        </div>
+                        <div className="event-data__row">
+                            <div className="event-data__row-title">Время: </div>
+                            <div className="event-data__row-content">{EventData.time} </div>
+                        </div>
+                    </div>
+                    <div className="event-data">
+                        <div className="event-data__row">
+                            <div className="event-data__row-title">Место:</div>
+                            <div className="event-data__row-content">{EventData.place} </div>
+                        </div>
+                    </div>
+                    <div className="event-data">
+                        <div className="event-data__row">
+                            <div className="event-data__row-title">Кол-во гостей:</div>
+                            <div className="event-data__row-content">{EventData.total_guests_count} </div>
+                        </div>
+                        <div className="event-data__row">
+                            <div className="event-data__row-title">Планируемый бюджет:</div>
+                            <div className="event-data__row-content">{EventData.budget} </div>
+                        </div>
+                    </div>
+                </Grid>
+                <Grid item xs={12} sm={6}>
+                    <div className="event-data">
+                        <h3 className="event-data__title">Ответственные</h3>
+                        {EventData.responsibles.map((item, index) =>
+                            <div className="event-data__row">
+                                <div className="event-data__row-title">
+                                    {item.title}
+                                </div>
+                                <div className="event-data__row-content">
+                                    {item.content}
+                                </div>
+                            </div>
+                        )}
+                    </div>
+                </Grid>
+            </Grid>
+        </section>
+    </div >
+)
 export default Home;
