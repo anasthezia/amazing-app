a2d91a182a869cc1b417e05c8031484d9f988f1e
anasthezia <anasthezia161@gmail.com>
2019-05-18 23:55:03 +0300

    Updates

---
+++ precache-manifest.02ce8e6570773354db626a6c4b5dcc21.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "f9f05ff79d4a65408abf",
+    "url": "//anasthezia.github.io/amazing-app/static/css/main.0cf6bf59.chunk.css"
+  },
+  {
+    "revision": "f9f05ff79d4a65408abf",
+    "url": "//anasthezia.github.io/amazing-app/static/js/main.6178349d.chunk.js"
+  },
+  {
+    "revision": "274a8c00be232913208d",
+    "url": "//anasthezia.github.io/amazing-app/static/js/runtime~main.4e5e75bd.js"
+  },
+  {
+    "revision": "0845b7e189422aec78f5",
+    "url": "//anasthezia.github.io/amazing-app/static/css/2.7deb1372.chunk.css"
+  },
+  {
+    "revision": "0845b7e189422aec78f5",
+    "url": "//anasthezia.github.io/amazing-app/static/js/2.fa62a01f.chunk.js"
+  },
+  {
+    "revision": "d7b900d70621efc83bcaae9069433b5d",
+    "url": "//anasthezia.github.io/amazing-app/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "//anasthezia.github.io/amazing-app/precache-manifest.02ce8e6570773354db626a6c4b5dcc21.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("//anasthezia.github.io/amazing-app/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
